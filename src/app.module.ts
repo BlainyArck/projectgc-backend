6aff461dab1eab881424aa851cd9d7a988eeb355
Alan Diogo "BlainyArck <36194828+BlainyArck@users.noreply.github.com>
2023-10-10 21:37:24 -0300

    feat: insert hash in password

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
-import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
+import { Module } from "@nestjs/common";
+import { UserModule } from "./user/user.module";
 
 @Module({
   imports: [UserModule],
---
+++ src/user/interfaces/user.interface.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  password: string;
+}
