c4a7654f56d0730199d06c6aa925746ac9e130f7
Alan Diogo "BlainyArck <36194828+BlainyArck@users.noreply.github.com>
2023-11-09 19:59:50 -0300

    feat: insert unit test in AuthService

---
+++ src/auth/__mocks__/jwt.mocks.ts
@@ -0,0 +1,2 @@
+export const jwtMock =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidHlwZVVzZXIiOjEsImlhdCI6MTY5OTU2ODg3OSwiZXhwIjoxNzAwMTczNjc5fQ.EuBGSXZ3UB6fytMUVhOBqtWJTd26BvKYex5yN_8SYiM';
--- src/user/__mocks__/user.mock.ts
+++ src/user/__mocks__/user.mock.ts
@@ -4,10 +4,10 @@ import { UserType } from '../enum/user-type.enum';
 export const userEntityMock: UserEntity = {
   cpf: '00000000000',
   createdAt: new Date(),
-  email: 'test@test.com',
+  email: 'root@root.com',
   id: 555777,
   name: 'nameMock',
-  password: 'largePassword',
+  password: '$2b$10$7FPH4inoOZvfsXR1.vsK9u5/MA5lpnGFtkbpZ84EYTH7jf2uMQ3k.',
   phone: '999999999',
   typeUser: UserType.User,
   updatedAt: new Date(),
