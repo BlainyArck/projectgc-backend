d037f07eee082c0e76cc477b30f8ff0cf189adab
Alan Diogo "BlainyArck <36194828+BlainyArck@users.noreply.github.com>
2023-11-10 18:48:34 -0300

    feat: insert unit test cache

---
+++ src/cache/__tests__/cache.service.spec.ts
@@ -0,0 +1,44 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CacheService } from '../cache.service';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { userEntityMock } from '../../user/__mocks__/user.mock';
+import { Cache } from 'cache-manager';
+
+describe('CacheService', () => {
+  let service: CacheService;
+  let cacheManager: Cache;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        CacheService,
+        {
+          provide: CACHE_MANAGER,
+          useValue: {
+            get: () => userEntityMock,
+            set: () => jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<CacheService>(CacheService);
+    cacheManager = module.get(CACHE_MANAGER);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should return in cache', async () => {
+    const user = await service.getCache('key', () => null);
+    expect(user).toEqual(userEntityMock);
+  });
+
+  it('should return data in function', async () => {
+    const result = { test: 'test' };
+    jest.spyOn(cacheManager, 'get').mockResolvedValue(undefined);
+    const user = await service.getCache('key', () => Promise.resolve(result));
+    expect(user).toEqual(result);
+  });
+});
