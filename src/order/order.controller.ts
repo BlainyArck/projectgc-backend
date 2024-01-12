9ee9d0cf60ce9c6410d05234fafe5fe6b7db5d1d
Alan Diogo "BlainyArck <alandiogo-10@hotmail.com>
2024-01-12 20:33:09 -0300

    feat: create function list all orders

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -10,7 +10,11 @@ import { CreateOrderDto } from './dtos/create-order.dto';
 import { OrderService } from './order.service';
 import { UserId } from '../decorators/user-id.decorator';
 import { OrderEntity } from './entities/order.entity';
+import { Roles } from '../decorators/roles.decorator';
+import { UserType } from '../user/enum/user-type.enum';
+import { ReturnOrderDto } from './dtos/return-order.dto';
 
+@Roles(UserType.Admin, UserType.User)
 @Controller('order')
 export class OrderController {
   constructor(private readonly orderSevice: OrderService) {}
@@ -27,5 +31,13 @@ export class OrderController {
   @Get()
   async findOrdersByUserId(@UserId() userId: number): Promise<OrderEntity[]> {
     return this.orderSevice.findOrdersByUserId(userId);
+  }
+
+  @Roles(UserType.Admin)
+  @Get('/all')
+  async findAllOrders(): Promise<ReturnOrderDto[]> {
+    return (await this.orderSevice.findAllOrders()).map(
+      (order) => new ReturnOrderDto(order),
+    );
   }
 }
